eb9c523a26d7c346100ebfb76dfc53dfe64674c6
uasmirnova <uasmirnova21@yandex.ru>
2022-07-21 14:40:32 +0300

    calculate v2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int amount = 1000000;
-        int period = 36;
-        float percent = 9.99F;
-        int creditPayment = service.calculate(amount, period, percent );
+
+        int creditPayment = service.calculate(1000000,12,9.99F);
+        System.out.println("Ежемесячный платеж = " + creditPayment + " руб");
+
+        creditPayment = service.calculate(1000000,24,9.99F);
+        System.out.println("Ежемесячный платеж = " + creditPayment + " руб");
+
+        creditPayment = service.calculate(1000000,36,9.99F);
         System.out.println("Ежемесячный платеж = " + creditPayment + " руб");
 
     }
